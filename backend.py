fe55419322a63a0ee05bcfb7dbf35327e827e0ca
SHARIGA P <sharigapandian24@gmail.com>
2025-06-03 12:37:28 +0530

    Complete development of App

--- backend.py
+++ backend.py
@@ -1,21 +1,16 @@
 import requests
-import os
-API_KEY= os.environ.get("API_KEY")
+API_KEY= "55ed09cccf55385d2af90eeeecaaa6f4"
 
 
-def get_data(name, days=None, type=None):
+
+def get_data(name, days):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={name}&appid={API_KEY}"
     response = requests.get(url)
     contents = response.json()
     filtered_data = contents['list']
     n_value = 8 * days
     filtered_data =  filtered_data[:n_value]
-
-    if type == "Temperature":
-        filtered_data = [data["main"]["temp"]for data in filtered_data]
-    if type == "Sky":
-        filtered_data = [data["weather"][0]["main"] for data in filtered_data]
     return filtered_data
 
 if __name__ == "__main__":
-    print(get_data(name="London",days=3,type="Sky"))
+    print(get_data(name="London",days=3))
--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
-st.title("Weather Forecast Dashboard🌦️")
+# ADD TITLE, SUBHEADER, INPUT_TEXT, SLIDER AND SELECTBOX
+
+st.set_page_config(page_title="Weather Forecast Dashboard",page_icon="🌦️",layout="wide",
+                   menu_items={"About":"This is a Weather Forecast Dahboard helps in displaying temperature and sky conditions upto 5 days!"})
+st.title("Welcome to Weather Forecast Dashboard🌦️")
 st.subheader("Explore the world's weather, one city at a time")
 st.write("A full-globe weather visualizer that lets you pick any country or city and view real-time temperature, Displays current weather conditions, 5-day forecast.")
 
@@ -10,18 +14,47 @@ city = st.text_input("Enter your city name")
 days = st.slider("Select Forecast days",min_value=1,max_value=5,help="Pick Number of days")
 view = st.selectbox("Select data to view", options=["Temperature","Sky"])
 submit = st.button("Submit")
+
+# AFTER USER INPUT
 if submit:
-    if days == 1:
-        day = "day"
+    if city:
+        try:
+            if days == 1:
+                day = "day"
+            else:
+                day = "days"
+            st.subheader(f"{view} for next {days} {day} in {city}")
+
+            # GET TEMPERATURE/SKY DATA
+            filtered_data= get_data(city, days)
+
+            # OUTPUT FOR TEMPERATURE
+            if view == "Temperature":
+                temperature = [data["main"]["temp"]/10 for data in filtered_data]
+                dates = [data["dt_txt"] for data in filtered_data]
+                figure = px.line(x=dates, y=temperature,labels={"x":"Date","y":"Temperature(C)"},line_shape="linear")
+                st.plotly_chart(figure)
+
+            # OUTPUT FOR SKY CONDITION
+            if view == "Sky":
+                description = [data["weather"][0]["description"].title() for data in filtered_data]
+                dates = [data["dt_txt"] for data in filtered_data]
+                captions = [f"{dest}\n\n{date}" for dest,date in zip(description, dates)]
+                filtered_data = [data["weather"][0]["main"] for data in filtered_data]
+                image = {"Clear": "images/clear.png",
+                         "Clouds": "images/cloud.png",
+                         "Rain": "images/rain.png",
+                         "Snow":"images/snow.png"}
+                image_path = [image[filter_data] for filter_data in filtered_data]
+                st.image(image=image_path, caption=captions, width=125,)
+
+        except KeyError:
+            st.error("Enter a valid city name")
+
+
     else:
-        day = "days"
-
-    st.subheader(f"{view} for next {days} {day} in {city}")
-    get_data(city, days, view)
-
-    figure = px.line(x=, y=,labels={"x":,"y":},line_shape="linear")
-    st.plotly_chart(figure)
+        st.error("Please enter a city name")
 
 
-st.write("Developed by \n"
-"[Sha-ri-cloud]")
+st.write(f"Developed by \n"
+f"[Sha-ri-cloud]")
